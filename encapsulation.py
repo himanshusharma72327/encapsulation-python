097391ed83133eb3654bce2da4e47e4b054f49e6
himanshusharma72327 <himanshusharma72327@gmail.com>
2025-11-27 10:38:03 +0530

    Add files via upload

---
+++ encapsulation.py
@@ -0,0 +1,54 @@
+# class Student:
+#     def __init__(self, name, roll, marks):
+#         self.name = name          # Public
+#         self._roll = roll         # Protected
+#         self.__marks = marks      # Private
+
+#     def show(self):
+#         print(f"Name: {self.name}, Roll: {self._roll}, Marks: {self.__marks}")
+
+#     def update_marks(self, marks):
+#         if 0 <= marks <= 100:
+#             self.__marks = marks
+#         else:
+#             print("Invalid Marks!")
+
+# student = Student("Amit", 101, 85)
+# student.show()
+# student.update_marks(93)
+# student.show()
+
+# sum=0
+# for i in range(2,21,2):
+#     sum=sum+i
+# print(sum)
+
+
+
+i=1
+while i<=10:
+    print(i*4)
+    i+=1    
+    
+# i=1
+# while i<=10:
+#     print(3*i)
+#     if(i==5) :   
+#      break
+#     i+=1  
+
+# # n=int(input("enter tne no"))
+# i=1
+# while i<=10:
+#     print(n*i)
+#     i+=1 
+
+
+# list=["cheery","apple","lichi"]
+# for i in list:
+#     print(i)
+# i=1
+# while i<=10:
+#   print(3*i)
+#   i+=1
+  
